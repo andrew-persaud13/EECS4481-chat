f8f63a1cd25852455d76cfd1084c50c76229f846
Andrew Persaud <andrew.persaud.dev@gmail.com>
2020-03-25 21:59:44 -0400

    deploying

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="insight-app-sec-validation" content="bea7cd8e-a7fc-4daa-9128-aebf984e8b33">
+    <meta name="insight-app-sec-validation" content="8a27078a-044d-4c41-ac99-17baca3cb959">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
